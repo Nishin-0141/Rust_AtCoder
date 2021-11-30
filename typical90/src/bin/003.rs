a45cfd6c7f1ad1a2e88ebef7fa0fe4f5442bf3db
Nishin-0141 <shinnosukepotato@gmail.com>
2021-11-30 18:11:33 +0900

    木の直径を求める関数を作成

--- typical90/src/bin/003.rs
+++ typical90/src/bin/003.rs
@@ -1,6 +1,57 @@
 use proconio::input;
 use std::vec;
-// 木の直径を求めるプログラム
+use std::collections::VecDeque;
+
+
+fn tree_diameter(n: usize, graph: Vec<Vec<usize>>) -> usize {
+    let num_max = 100000000;
+    
+    // First BFS
+    let mut q: VecDeque<usize> = VecDeque::new();
+    let mut dist = vec![num_max; n];
+    q.push_back(0);
+    dist[0] = 0;
+    while q.len() > 0 {
+        let v = q.pop_front().unwrap();
+        for i in 0..graph[v].len() {
+            let to = graph[v][i];
+            if dist[to] != num_max {
+                continue;
+            }
+            dist[to] = dist[v] + 1;
+            q.push_back(to);
+        }
+    }
+    
+    // Search longest path
+    let mut num: [usize; 2] = [0; 2];
+    for i in 0..n {
+        if num[0] < dist[i] {
+            num[0] = dist[i];
+            num[1] = i;
+        }
+    }
+    
+    // Second BFS
+    let mut q: VecDeque<usize> = VecDeque::new();
+    let mut dist = vec![num_max; n];
+    q.push_back(num[1]);
+    dist[num[1]] = 0;
+    while q.len() > 0 {
+        let v = q.pop_front().unwrap();
+        for i in 0..graph[v].len() {
+            let to = graph[v][i];
+            if dist[to] != num_max {
+                continue;
+            }
+            dist[to] = dist[v] + 1;
+            q.push_back(to);
+        }
+    }
+    return *dist.iter().max().unwrap();
+}
+
+
 fn main() {
     input! {
         n: usize,
@@ -16,77 +67,6 @@ fn main() {
         graph[i[0] - 1].push(i[1] - 1);
         graph[i[1] - 1].push(i[0] - 1);
     }
-    let dist_first = bfs(graph, 0, n);
-    let mut num = 0;
-    let mut num_index = 0;
-    for i in 0..n {
-        if num < dist_first[i] {
-            num = dist_first[i];
-            num_index = i;
-        }
-    }
-    let dist_second = bfs(graph, num_index, n);
-    println!("{}", dist_second.iter().max().unwrap());
-    /*
-    // 1回目のBFS
-    let mut q: Vec<usize> = Vec::new();
-    let mut dist: Vec<usize>= vec![100000000; n];
-    q.push(0);
-    dist[0] = 0;
-    while q.len() > 0 {
-        let v = q.pop().unwrap();
-        for i in 0..graph[v].len() {
-            let to = graph[v][i];
-            if dist[to] != 100000000 {
-                continue;
-            }
-            dist[to] = dist[v] + 1;
-            q.push(to);
-        }
-    }
-    // println!("{:?}", dist);
-
-    // 2回目のBFS
-    let mut num: [usize; 2] = [0; 2];
-    for i in 0..n {
-        if num[0] < dist[i] {
-            num[0] = dist[i];
-            num[1] = i;
-        }
-    }
-    let mut q: Vec<usize> = Vec::new();
-    let mut dist= vec![100000000; n];
-    q.push(num[1]);
-    dist[num[1]] = 0;
-    while q.len() > 0 {
-        let v = q.pop().unwrap();
-        for i in 0..graph[v].len() {
-            let to = graph[v][i];
-            if dist[to] != 100000000 {
-                continue;
-            }
-            dist[to] = dist[v] + 1;
-            q.push(to);
-        }
-    }
-    println!("{}", dist.iter().max().unwrap() + 1)
-    */
-}
-fn bfs(graph: Vec<Vec<usize>>, start: usize, n: usize) -> Vec<i32>{
-    let mut q: Vec<usize> = Vec::new();
-    let mut dist: Vec<i32> = vec![100000000; n];
-    q.push(start);
-    dist[start] = 0;
-    while q.len() > 0 {
-        let v = q.pop().unwrap();
-        for i in 0..graph[v].len() {
-            let to = graph[v][i];
-            if dist[to] != 100000000 {
-                continue;
-            }
-            dist[to] = dist[v] + 1;
-            q.push(to);
-        }
-    }
-    return dist;
+    let ans = tree_diameter(n, graph) + 1;
+    println!("{}", ans);
 }
